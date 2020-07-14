dac9aa5a7500883b36c6fe31186f34806021adda
senaybee <senay_berihu@outlook.com>
2020-07-13 17:04:30 -0700

    Add a builder function that returns a Container composed of a controlled TextField.

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,35 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('FriendlyChat'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('FriendlyChat'),
+        ),
+        body: _buildTextComposer());
   }
 }
